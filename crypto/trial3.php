f5b585d8fc9ae55f0a204c014da2d2bf1acf0162
Özkan Gökdere <ozkangokdere@gmail.com>
2018-01-09 23:39:57 +0300

    in order to transfer

---
+++ crypto/trial3.php
@@ -0,0 +1,40 @@
+<?php
+require '../vendor/autoload.php';
+
+$ticker = $api->prices();
+$keys = array_keys($ticker);
+
+$start = 37;
+
+while(true) {
+    $flag = true;
+    $prev = -1;
+    $count = 0;
+    foreach($keys as $key) {
+        if(strpos($key, "BTC") == false) {
+            continue;
+        }
+
+        if ($count >= $start) {
+            if ($flag) {
+                $ticks = $api->candlesticks($key, "1m");
+                end($ticks);
+                $tick =  prev($ticks);
+                $now = intval($tick["openTime"]);
+                if ($now != $prev) {
+                    if($prev != -1)
+                        $flag = false;
+                }
+            $prev = $now;
+            echo $key."\t".$tick["open"]."\t".$tick["volume"]."\t".$tick["openTime"].PHP_EOL;
+        }
+            
+        }
+        $count = $count + 1;
+    }
+
+    
+}
+
+
+?>
